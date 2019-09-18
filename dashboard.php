e6ad64962cd815ce64c11c3c1eb168bdff7256d6
Kazeem Asifat <35298707+Kazeeem@users.noreply.github.com>
2019-09-18 21:00:20 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,25 +1,39 @@
 <?php
-	session_start();
+    session_start();
 
-	if (isset($_SESSION['user_logged_in']) == false) {
-		header('Location: login.php');
-	} else {
-		$username = $_SESSION['user'];
-		$mail = $_SESSION['email'];
-	}
+    if (isset($_SESSION['user_logged_in']) == false) {
+        header('Location: index.php');
+    } else {
+        $username = $_SESSION['user'];
+        $mail = $_SESSION['email'];
+    }
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>User Dashboard</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/dashboard.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto&display=swap" rel="stylesheet">
+    <title>Welcome to CodAvengers</title>
+    <!-- 
+        Designed by @StephanieOgbudu and @Faith Egwuenu,
+        Front End Developed By @abby_joe, tekipharm, @localhost and @ElijahWale,
+        Back End Developed By @Kazeem Asifat, @ibeFx and @Queue
+    -->
 </head>
 <body>
-	<div class="box">
-		<h3>Hello <?php echo $username; ?>, welcome to Codavengers Task</h3>
-		<p>Your Email is: <?php echo $mail; ?></p>
-	</div>
-
-	<a href="<?php session_destroy(); ?>">Logout</a>
+    <header>
+        <img src="images/logo.svg" alt="logo">  
+    </header>
+    <section>
+        <p>Welcome <span class="user"><?php echo $username; ?></span>,</p>
+        <h2>This is the Codavengers Hub</h2>
+        <h3>Playground for Designers, Front-End Developers & <br> Back-End Developers.</h3>
+        <p>Your Email is: <?php echo $mail; ?></p>
+        <a href="logout.php">Logout</a>
+    </section>
 </body>
 </html>
